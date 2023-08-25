0f29878df2084061261a349123d58e5b415e770a
sa-walker <141259377+sa-walker@users.noreply.github.com>
2023-08-25 12:30:01 -0500

    Added in logic to get the item and warehouse entities for an inventoryitemDto

--- SpringBoot/project1/src/main/java/com/skillstorm/mappers/InventoryItemMapper.java
+++ SpringBoot/project1/src/main/java/com/skillstorm/mappers/InventoryItemMapper.java
@@ -1,5 +1,10 @@
 package com.skillstorm.mappers;
 
+import javax.persistence.Transient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.skillstorm.dtos.InventoryItemRequestDto;
@@ -7,10 +12,20 @@ import com.skillstorm.dtos.InventoryItemResponseDto;
 import com.skillstorm.models.InventoryItem;
 import com.skillstorm.models.Item;
 import com.skillstorm.models.Warehouse;
+import com.skillstorm.repositories.ItemRepository;
+import com.skillstorm.repositories.WarehouseRepository;
 
 @Component
 public class InventoryItemMapper {
+	
+	@Transient
+	Logger logger = LoggerFactory.getLogger(getClass());
 
+	@Autowired
+	private ItemRepository itemRepo;
+	
+	@Autowired
+	private WarehouseRepository wareRepo;
 	// convert a InventoryItem Entity into a InventoryItemResponseDto
 
 	public InventoryItemResponseDto convert(InventoryItem inventoryItem) {
@@ -23,10 +38,24 @@ public class InventoryItemMapper {
 	// convert a InventoryItemRequestDto into a InventoryItem Entity
 	
 	public InventoryItem convert(InventoryItemRequestDto inventoryItemDto) {
+		
+		Item item = itemFromId(inventoryItemDto.getItemId());
+		Warehouse warehouse = warehouseFromId(inventoryItemDto.getWarehouseId());
+		
 		return new InventoryItem(inventoryItemDto.getId(),
-				new Item(inventoryItemDto.getItemId()),
-				new Warehouse(inventoryItemDto.getWarehouseId()),
+				item,
+				warehouse,
 				inventoryItemDto.getQuantity());
 	}
+	
+	public Item itemFromId(int id) {
+		logger.debug("item id is " + id);
+        return itemRepo.findById(id).orElse(new Item());
+    }
+
+	public Warehouse warehouseFromId(int id) {
+		logger.debug("warehouse id is " + id);
+        return wareRepo.findById(id).orElse(new Warehouse());
+    }
 
 }
